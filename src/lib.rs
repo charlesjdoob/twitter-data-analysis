2449bbae48fe8bd8ccb9277fa7df74a8c42a8baa
Charles <81934710+charlesj420@users.noreply.github.com>
2021-04-22 15:48:16 -0700

    Make pruning more strict, optimize code slightly

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub mod twitter_utils {
+    use std::io::BufWriter;
     use std::iter::FromIterator;
     use std::fs::File;
     use std::io::prelude::*;
@@ -6,25 +7,37 @@ pub mod twitter_utils {
     use std::path::Path;
     use std::collections::HashMap;
     use std::time::Instant;
-
+    const TOP_USERS_COEFFICIENT: f64 = 0.10;
+    const TOP_USERS_FILE: &str = "top-users-result";
+    const PATH: &str = "/home/user/Documents/hello/files/";
     pub fn get_top_users(input_file_name: &str, num_iterations: u32) {
         let mut now = Instant::now();
         let mut curr_iteration = 0;
-        let mut top_users: HashMap<u64, u64> = HashMap::<u64, u64>::new();
         let mut pruned_file_name: String;
         let mut curr_input_file: String =(*input_file_name).to_string();
+        let mut top_users_file_name: String;
         while curr_iteration < num_iterations {
-            top_users = count_followers(&curr_input_file); 
+            let mut result: Vec<(&u64, &u64)>;
+            let top_users: HashMap<u64, u64> = count_followers(&curr_input_file);
             pruned_file_name = format!("{}.pruned{}", curr_input_file, curr_iteration);
             prune_data(&curr_input_file, &pruned_file_name, &top_users); 
             curr_input_file = pruned_file_name; 
             println!("Time for iteration {}: {:?}", curr_iteration, now.elapsed());
             now = Instant::now();
             curr_iteration += 1;
+            top_users_file_name = format!("{}{}{}.txt", PATH, TOP_USERS_FILE, curr_iteration);
+            let path = Path::new(&top_users_file_name);
+            let mut file = match File::create(&path) {
+                Err(why) => panic!("couldn't create {}", why),
+                Ok(file) => BufWriter::new(file),
+            };
+            result = Vec::from_iter(top_users.iter());
+            result.sort_by(|a, b| b.1.cmp(&a.1));
+            match file.write_all(format!("{:?}", result).as_bytes()) {
+                Err(why) => panic!("couldn't write to {}", why),
+                Ok(_) => ()
+            }
         }
-        let mut result = Vec::from_iter(top_users.iter());
-        result.sort_by(|a, b| b.1.cmp(&a.1));
-        println!("Top users: \n{:?}", result);
     }
     pub fn count_followers(input_file_name: &str) -> HashMap<u64, u64> {
         let mut lines_read = 0;
@@ -46,7 +59,7 @@ pub mod twitter_utils {
                     };
                     followers.insert(user_id, num_followers);
                     
-                    if lines_read % 10000000 == 0 {
+                    if lines_read % 100000000 == 0 {
                         println!("read {} lines!", lines_read);
                     }
                     lines_read += 1;
@@ -58,48 +71,59 @@ pub mod twitter_utils {
         //Create a vec of tuples so I can sort all the stuff because I am lazy and stupuid
         let mut followers_vec = Vec::from_iter(followers.iter());
         followers_vec.sort_by(|a, b| b.1.cmp(&a.1));
-        //followers_vec.drain(((followers_vec.len() * 0.9) as usize)..);
-        followers_vec.truncate((followers_vec.len() as f64 * 0.1).round() as usize);
+        followers_vec.truncate(((followers_vec.len() as f64) * TOP_USERS_COEFFICIENT).round() as usize);
         let result = followers_vec.into_iter().map(|a| (*a.0, *a.1)).collect(); 
         result
     }
     
+    // Create new file from previous input data which only contains lines where both users are in
+    // the users_to_keep map
     fn prune_data(input_file_name: &str, output_file_name: &str, users_to_keep: &HashMap<u64, u64>) { 
         let mut now = Instant::now();
         let mut lines_read = 0;
+
         if let Ok(lines) = read_lines(input_file_name) {
-            let path = Path::new(&output_file_name);
-            let mut file = match File::create(&path) {
+            let mut file = match File::create(&Path::new(&output_file_name)) {
                 Err(why) => panic!("couldn't create {}", why),
-                Ok(file) => file,
+                Ok(file) => BufWriter::new(file),
             };
+            let mut prev_second_user: u64 = u64::MAX;
+            let mut keep_second_user: bool = false;
             for line in lines {
                 if let Ok(ip) = line {
                     let mut line_iter = ip.split_whitespace();
-                    let first_user_id : u64 = match line_iter.next() {
-                        Some(id) => id.parse::<u64>().unwrap(),
-                        None => 0
+                    let first_user_id_unparsed = match line_iter.next() {
+                        Some(id) => id,
+                        None => ""
                     };
                     let second_user_id : u64 = match line_iter.next() {
                         Some(id) => id.parse::<u64>().unwrap(),
                         None => 0
                     };
-                    let mut keep_line = false;
-                    match users_to_keep.get(&first_user_id) {
-                        Some(_) => keep_line = true,
-                        None => match users_to_keep.get(&second_user_id) {
-                            Some(_) => keep_line = true,
-                            None => ()
-                        }
+                    if second_user_id != prev_second_user {
+                        prev_second_user = second_user_id;
+                        keep_second_user = match users_to_keep.get(&second_user_id) {
+                            Some(_) => true,
+                            None => false
+                        };
                     }
-                    if keep_line { 
+                    let mut keep_first_user: bool = false;
+                    if keep_second_user {
+                        let first_user_id = first_user_id_unparsed.parse::<u64>().unwrap();
+                        keep_first_user = match users_to_keep.get(&first_user_id) {
+                            Some(_) => true,
+                            None => false
+                        };
+                    }
+                    
+                    if keep_first_user && keep_second_user { 
                         match file.write_all(format!("{}\n", ip).as_bytes()) {
                             Err(why) => panic!("couldn't write to {}", why),
                             Ok(_) => ()
                         }
                     }
                 }
-                if lines_read % 1000000 == 0 {
+                if lines_read % 100000000 == 0 {
                     println!("Read {} lines in {:?}", lines_read, now.elapsed());
                     now = Instant::now();
                 }
@@ -114,7 +138,10 @@ pub mod twitter_utils {
     // Returns an Iterator to the Reader of the lines of the file.
     fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
     where P: AsRef<Path>, {
-        let file = File::open(filename)?;
+        let file = match File::open(filename) {
+            Err(why) => panic!("couldn't open {}", why),
+            Ok(file) => file,
+        };
         Ok(io::BufReader::new(file).lines())
     }
 }
